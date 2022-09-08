ec24a2ee4d8e22ca983126fbd382a6c7635e361f
Adda1321 <adilmustafa13@gmail.com>
2022-09-09 00:44:33 +0500

    afterblunder

--- src/components/RequiredAuth.js
+++ src/components/RequiredAuth.js
@@ -1,10 +1,11 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { useAuth } from './auth'
-
 export const RequireAuth = ( ) => {
   const location = useLocation()
   const auth = useAuth()
   return(
+    
   auth.user ? <Outlet/> :    <Navigate to='/login' state={{ path: location.pathname }} />
   ) 
-} //testing
+  
+} //
